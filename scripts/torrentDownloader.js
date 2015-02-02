8c02092afcff3301565ab692c8c0da18b36195f3
Antryg <antryg666@gmail.com>
2015-02-02 01:25:38 +0100

    Promises and Restructure

--- scripts/torrentDownloader.js
+++ scripts/torrentDownloader.js
@@ -9,121 +9,168 @@ var ping          = require('ping');
 var system        = require(__dirname + "/../util/system.js");
 var diskspace     = require('diskspace');
 var fs            = require('fs');
-
-var diskSpaceLimit = 500 * 1024;
-var openActions = 0;
-var hostname = 'Odin';
-var systemfolder = '/tmp/torrent-stream/';
-var hostfolder   = '/mnt/odin/video/anime/';
+var Q             = require('q');
 
 log4js.configure( __dirname + '/../config/torrent_downloader.json', { reloadSecs: 300 });
 system.execute("rm -rf /tmp/torrent-stream/*");
 
 console.log('starting...');
 
-diskspace.check('/', function (err, total, free, status) {
+start();
 
-    if( free < diskSpaceLimit ) {
-        console.error('...not enough space left on device');
-        closeAction();
-    } else {
+function start() {
 
-        MongoClient.connect('mongodb://127.0.0.1:27017/jarvis', function(err, db) {
+    var self = this;
 
-            if (err) throw err;
+    self.diskSpaceLimit = 500 * 1024;
+    self.openActions = 0;
+    self.hostname = 'Odin';
+    self.systemfolder = '/tmp/torrent-stream/';
+    self.hostfolder   = '/mnt/odin/video/anime/';
 
-            var episodesCollection = db.collection('episodes');
-            var tvseriesCollection = db.collection('tvseries');
+    diskspace.check('/', function (err, total, free, status) {
 
-            episodesCollection.find({ queue : true }).toArray( function( err, data ) {
+        if (free < diskSpaceLimit) {
+            console.error('...not enough space left on device');
+            self.closeAction();
+        } else {
 
-                var openActions = data.length;
-                if( openActions == 0 ) {
-                    console.log('...nothing to do');
-                    closeAction();
-                }
+            MongoClient.connect('mongodb://127.0.0.1:27017/jarvis', function (err, db) {
 
-                if( openActions > 0 ) {
+                if (err) throw err;
 
-        //            for( var episode in data ) {
-                    var episode = data[0];
-                    if(true) {
+                self.episodesCollection = db.collection('episodes');
+                self.tvseriesCollection = db.collection('tvseries');
 
-                        tvseriesCollectiontvseriesCollection.find({ 'id' : episode.tvid}).toArray(function( err, data ) {
+                self.episodesCollection.find({ queue: true }).toArray(function (err, data) {
 
-                            if( data.length == 0 ) {
-                                console.error('...no tvseries to episode found: ' + episode.tvid);
-                                closeAction();
-                            } else {
+                    self.openActions += data.length;
 
-                                var engine = torrentStream(episode.magnet);
-                                var tvseries = data[0];
+                    if (openActions == 0) {
+                        console.log('...nothing to do');
+                        self.closeAction();
+                    }
 
-                                engine.on('ready', function () {
-                                    engine.files.forEach(function (file) {
+                    if (openActions > 0) {
 
-                                        console.log('...streaming: ' + file.name);
+                        //            for( var episode in data ) {
+                        var episode = data[0];
+                        if (true) {
 
-                                        var stream = file.createReadStream();
+                            self.tvseriesCollection.find({ 'id': episode.tvid}).toArray(function (err, data) {
+                                if (data.length == 0) {
+                                    console.error('...no tvseries to episode found: ' + episode.tvid);
+                                    self.closeAction();
+                                } else {
+                                    self.downloadTorrent(episode, data[0])
+                                        .then(self.finalizeTorrent)
+                                        .then(self.moveTorrent);
+                                }
+                            });
 
-                                        stream.on('data', function () {
-                                        });
-
-                                        stream.on('end', function () {
-
-                                            console.log('...finished: ' + file.name);
-
-                                            ping.sys.probe(hostname, function (isAlive) {
-                                                if (isAlive) {
-                                                    fs.rename(
-                                                        systemfolder + engine.infoHash + '/' + file.name,
-                                                        hostfolder + '/' + tvseries.desc + "/" + file.name,
-                                                        function (err) {
-                                                            console.log('...copy failed of: ' + file.name);
-                                                            console.error(err);
-                                                            closeAction();
-                                                        }
-                                                    );
-                                                } else {
-                                                    console.log('...host (' + hostname + ') is down');
-                                                    closeAction();
-                                                }
-                                            });
-
-                                            //                            engine.destroy();
-
-                                        });
-
-                                    });
-                                });
-
-
-                                engine.on('download', function (index) {
-                                    console.log((' Piece: ' + index + ' downloaded'));
-                                });
-
-                            }
-
-                        });
+                        }
 
                     }
 
-                }
+                });
 
             });
 
+        }
+    });
+
+    self.downloadTorrent = function( episode, tvseries ) {
+
+        var deferred = Q.defer();
+        var engine = torrentStream(episode.magnet);
+
+        engine.on('ready', function () {
+            engine.files.forEach(function (file) {
+
+                console.log('...streaming: ' + file.name);
+
+                var stream = file.createReadStream();
+
+                stream.on('data', function () {});
+
+                stream.on('end', function () {
+                    console.log('...finished: ' + file.name);
+                    engine.destroy();
+//                    finalizeTorrent(tvseries, file);
+                    deferred.resolve(tvseries, file);
+                });
+
+            });
         });
 
-    }
-});
+        engine.on('download', function (index) {
+            console.log((' Piece: ' + index + ' downloaded'));
+        });
 
-    function closeAction() {
+        return deferred.promise;
+
+    };
+
+    self.finalizeTorrent = function(tvseries, file) {
+
+        var deferred = Q.defer();
+        ping.sys.probe(hostname, function (isAlive) {
+            if (isAlive) {
+
+                fs.exists(hostfolder + '/' + tvseries.desc, function (exists) {
+                    if (exists) {
+//                        moveTorrent(file);
+                        deferred.resolve(file);
+                    } else {
+                        fs.mkdir(hostfolder + '/' + tvseries.desc, function (err) {
+                            if (err) {
+                                console.log(err);
+                                self.closeAction();
+                            }
+//                            moveTorrent(file);
+                            deferred.resolve(file);
+                        });
+                    }
+                });
+
+            } else {
+                console.log('...host (' + hostname + ') is down');
+                self.closeAction();
+            }
+        });
+
+        return deferred.promise;
+
+    };
+
+    self.moveTorrent = function(file) {
+
+        fs.rename(
+                systemfolder + engine.infoHash + '/' + file.name,
+                hostfolder + '/' + tvseries.desc + "/" + file.name,
+            function (err) {
+
+                if (err) {
+                    console.log('...copy failed of: ' + file.name);
+                    console.error(err);
+                    self.closeAction();
+                }
+
+
+            }
+        );
+
+    };
+
+    self.closeAction = function() {
         openActions--;
-        if( openActions < 1 ) {
+        if (openActions < 1) {
             db.close();
             console.log('...shutting down');
-            log4js.shutdown(function() {
+            log4js.shutdown(function () {
                 process.exit(1);
             });
         }
-    }
+    };
+
+}
